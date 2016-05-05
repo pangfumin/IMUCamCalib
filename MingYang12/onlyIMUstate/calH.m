93db4063ea83c35aa1dae644901ad4764023a33f
pangfumin <pangfumin@gmail.com>
2016-05-05 22:54:21 +0800

    add extrinsic

--- MingYang12/onlyIMUstate/calH.m
+++ MingYang12/onlyIMUstate/calH.m
@@ -4,9 +4,9 @@ H = [];
 for i = 1:validPointsNum
     % calculate 3d position in camera frame
     P_f_G = checkerBoardPoints(:,validIndex(i));
-    R_C_I = quatToRotMat(ekfState.q_C_I);
+    R_C_I = camera.R_C_I;
     R_I_G = quatToRotMat(ekfState.q_I_G);
-    P_f_C = R_C_I*R_I_G*(P_f_G - ekfState.p_I_G) + ekfState.p_I_C;
+    P_f_C = R_C_I*R_I_G*(P_f_G - ekfState.p_I_G)  - R_C_I*camera.Ip_C;
     X = P_f_C(1);
     Y = P_f_C(2);
     Z = P_f_C(3);
@@ -16,12 +16,9 @@ for i = 1:validPointsNum
     H_theta_i = Jcam*R_C_I*R_I_G*crossMat(P_f_G - ekfState.p_I_G);
     
     H_p_i = -Jcam*R_C_I*R_I_G;
-    
-    PI_phi_i = Jcam*R_C_I*crossMat(R_I_G*(P_f_G - ekfState.p_I_G));
-    PI_p_i = Jcam;
   
     
-    Hi = [H_theta_i H_p_i zeros(2,9) PI_phi_i PI_p_i];
+    Hi = [H_theta_i H_p_i zeros(2,9)];
     H = [H;
         Hi];
  
